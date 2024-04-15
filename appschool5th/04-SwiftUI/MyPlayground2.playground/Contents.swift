f26ce956975be66556093d0d8aed8579e0dbbbd4
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-15 11:16:29 +0900

    opaque return type

--- appschool5th/04-SwiftUI/MyPlayground2.playground/Contents.swift
+++ appschool5th/04-SwiftUI/MyPlayground2.playground/Contents.swift
@@ -16,4 +16,25 @@ class Myclass: MessageBuilder {
     func buildMessage () -> String {
         return "Hello" + name //반환타입과 마지막 표현식이 일치하면 return 생략 가능
     }
+    
 }
+
+func doubleFunc1 (value: Int) -> some Equatable { //Equatable은 프로토콜, some반환타입을 명확하게 알려주기 싫어서
+    return value * 2
+}
+
+func doubleFunc2 (value: String) -> some Equatable {
+    value + value
+}
+
+let intOne = doubleFunc1(value: 10)
+print(intOne)
+let stringOne = doubleFunc2(value: "Hello")
+print(stringOne)
+
+/*
+//동작하지 않는다, 불투명 반환타입
+if(intOne == stringOne) {
+    print("Tjey match")
+}
+*/
