f963a9c8eed9e2bb21749e82a3e8bd6e47242dbc
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-15 09:35:16 +0900

    class property method

--- appschool5th/04-SwiftUI/MyPlayground.playground/Contents.swift
+++ appschool5th/04-SwiftUI/MyPlayground.playground/Contents.swift
@@ -11,11 +11,23 @@ class 클래스 이름: 부모 클래스 {
 */
 
 class BankAccount {
+    //프로퍼티
     var accountBlance: Float = 0
     var accountNumber: Int = 0
+    
+    //인스턴스 메서드
+    func displayBlance() {
+        print("Number \(accountNumber)")
+        print("Current balance is \(accountBlance)")
+    }
+    //class 타입 메서드 상속 재정의 가능, static는 재정의 불가능
+    class func getMaxBalance() -> Float {
+        return 100000.00
+    }
 }
 
 var bankAccount = BankAccount()
 
 print(bankAccount.accountBlance)
 print(bankAccount.accountNumber)
+bankAccount.displayBlance()
