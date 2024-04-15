db06ce67248038617e163e83434e75f06fd4caec
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-15 10:01:31 +0900

    computed property

--- appschool5th/04-SwiftUI/MyPlayground.playground/Contents.swift
+++ appschool5th/04-SwiftUI/MyPlayground.playground/Contents.swift
@@ -14,6 +14,31 @@ class BankAccount {
     //프로퍼티
     var accountBlance: Float = 0
     var accountNumber: Int = 0
+    let fees: Float = 25.00
+    
+    var balanceLessFees: Float {
+        get {
+            return accountBlance - fees
+        }
+    }
+   
+    /*
+    init() {
+        accountBlance = 0
+        accountNumber = 0
+    }
+    */
+    
+    //이니셜라이저
+    init(number: Int, balance: Float) {
+        accountNumber = number
+        accountBlance = balance
+    }
+    
+    deinit {
+        //deinit을 만들어줘야지 안정적인 프로그램이됨
+        //필요한 정리작업수행
+    }
     
     //인스턴스 메서드
     func displayBlance() {
