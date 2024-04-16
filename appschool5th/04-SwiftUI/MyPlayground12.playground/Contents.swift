cd74e9e685bca6b8edc33fc6108ce663d6f5a5fe
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-16 09:44:44 +0900

    Struct, Class

---
+++ appschool5th/04-SwiftUI/MyPlayground12.playground/Contents.swift
@@ -0,0 +1,40 @@
+
+//구조체는 복사, 프로토콜을 이용해서 확장해나감
+struct SampleStruct {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func buildHelloMsg() -> String{
+        return "Hello" + name
+    }
+}
+
+class SampleClass {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func buildHelloMsg() -> String{
+        return "Hello" + name
+    }
+}
+
+let myStruct1 = SampleStruct(name: "Mark")
+var myStruct2 = myStruct1
+myStruct2.name = "Daivd"
+
+print(myStruct1.name)
+print(myStruct2.name)
+
+//클래스는 해당 주소 값에 가서 주소 값을 바꿔줌 그래서 다변한거, 클래스는 참조, 상속하면서 확장해나감
+let myClass1 = SampleClass(name: "Mark")
+var myClass2 = myClass1
+myClass2.name = "Daivd"
+
+print(myClass1.name)
+print(myClass2.name)
