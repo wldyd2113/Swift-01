eb7beaee52b1b22cc8b52dc63a3f0220e4f28734
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-16 11:16:16 +0900

    Array

--- appschool5th/04-SwiftUI/MyPlayground14.playground/Contents.swift
+++ appschool5th/04-SwiftUI/MyPlayground14.playground/Contents.swift
@@ -1,3 +1,34 @@
-import UIKit
 
-var greeting = "Hello, playground"
+//var treeArray: [String] = ["Pine", "Oak", "Yew"]
+var treeArray = ["Pine", "Oak", "Yew"] //타입 추론으로 생략
+
+var priceArray = [Float]() //Empty Array
+
+var nameArray = [String](repeating: "MyString", count: 10)
+
+let firstArray = ["Red", "Green", "Blue"]
+let secondArray = ["Indigo", "Violet"]
+
+let thirArray = firstArray + secondArray
+
+print(thirArray.count) //배열의 요소 개수
+
+print(treeArray.isEmpty)//Empty Array인지 확인
+
+print(treeArray[2]) //배열 요소 접근
+
+//요소 추가
+//treeArray[1] = "Redwood"
+treeArray.append("Redwood")
+//treeArray += ["Redwood"]
+
+//treeArray = treeArray + ["Maple", "Birch"]
+treeArray += ["Maple", "Birch"]
+
+
+treeArray.shuffle()
+
+print(treeArray)
+
+print(treeArray.randomElement() ?? "") //배열 random함수
+
