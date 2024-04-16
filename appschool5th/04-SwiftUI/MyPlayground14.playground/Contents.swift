aa49f9a2bc9415450600a411697351712acef60f
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-16 11:44:46 +0900

    Dictionary

--- appschool5th/04-SwiftUI/MyPlayground14.playground/Contents.swift
+++ appschool5th/04-SwiftUI/MyPlayground14.playground/Contents.swift
@@ -32,3 +32,24 @@ print(treeArray)
 
 print(treeArray.randomElement() ?? "") //배열 random함수
 
+
+//var bookDict:[String: String] = ["100-432112": "Wind in the Willows"]
+var bookDict1 = ["100-432112": "Wind in the Willows"]
+
+//Empty Dictionary 초기화 정수형 key값, 문자열
+var myDictionary = [Int: String]()
+
+let keys = ["1","2","3","4","5","6","7"]
+let values = ["Value1","Value2","Value3","Value4","Value5","Value6","Value7"]
+
+//let bookDict = Dictionary(uniqueKeysWithValues: zip(keys, values)) //순서는 보장 되지않음
+var bookDict = Dictionary(uniqueKeysWithValues: zip(1..., values))
+
+print(bookDict.count)
+//print(bookDict[1, default: "NoVaule"] )
+print(bookDict[1] ?? "NoVaule")
+bookDict[1231000] = "Value"
+
+for(bookId, value) in bookDict {
+    print("Book Id: \(bookId) Value: \(value)")
+}
