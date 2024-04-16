b775d8440a1aecdd5456cb1c481e5687ba47a57f
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-16 10:20:32 +0900

    Property Wrapper

--- appschool5th/04-SwiftUI/MyPlayground13.playground/Contents.swift
+++ appschool5th/04-SwiftUI/MyPlayground13.playground/Contents.swift
@@ -1,3 +1,34 @@
-import UIKit
 
-var greeting = "Hello, playground"
+@propertyWrapper
+struct FixCase {
+    private(set) var value: String = ""
+    
+    var wrappedValue: String {
+        get { value }
+        set { value = newValue.uppercased() }
+    }
+    init(wrappedValue initialValue: String) {
+        self.wrappedValue = initialValue
+    }
+}
+
+struct Contact {
+    @FixCase var name: String
+    @FixCase var city: String
+    @FixCase var country: String
+}
+
+var contact = Contact(name: "John Smith", city: "London", country: "United Kingdom")
+print("\(contact.name), \(contact.city), \(contact.country)")
+
+struct Address {
+    private var cityname: String = ""
+    var city: String {
+        get { cityname }
+        set { cityname = newValue.uppercased() }
+    }
+}
+
+var addresss = Address()
+addresss.city = "London"
+print(addresss.city)
